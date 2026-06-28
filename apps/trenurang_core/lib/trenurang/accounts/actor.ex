54955bdd0c80e99d3345883ed72ba46287f4c680
agungramadhan91 <agungramadhans.work@gmail.com>
2026-06-28 15:23:14 +0700

    feat(slice-1): schema Trenurang.Accounts.Actor + validasi legal_form/mandate_type orthogonality

---
+++ apps/trenurang_core/lib/trenurang/accounts/actor.ex
@@ -0,0 +1,57 @@
+defmodule Trenurang.Accounts.Actor do
+  @moduledoc """
+  Peran ekonomi, mengikuti klasifikasi UN SNA -- TERPISAH dari identitas
+  (User) dan dari objek katalog yang dicocokkan (Persona/Produk/AgendaProgram).
+
+  `legal_form` dan `mandate_type` ORTOGONAL, bukan satu badge gabungan:
+  `legal_form` cuma valid kalau type == :bisnis, `mandate_type` cuma valid
+  kalau type == :organisasi. Koperasi masuk :bisnis (legal_form: :koperasi),
+  bukan :organisasi.
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  schema "actors" do
+    field :type, Ecto.Enum, values: [:human, :bisnis, :organisasi]
+    field :legal_form, Ecto.Enum, values: [:perorangan, :cv, :pt, :koperasi]
+    field :mandate_type, Ecto.Enum, values: [:yayasan, :perkumpulan, :lembaga_pemerintah]
+    field :display_name, :string
+
+    belongs_to :user, Trenurang.Accounts.User
+    has_one :persona, Trenurang.Catalog.Persona
+    has_many :produks, Trenurang.Catalog.Produk
+    has_many :agenda_programs, Trenurang.Catalog.AgendaProgram
+    has_many :intents, Trenurang.Matching.Intent
+
+    timestamps()
+  end
+
+  @type t :: %__MODULE__{}
+
+  def changeset(actor, attrs) do
+    actor
+    |> cast(attrs, [:user_id, :type, :legal_form, :mandate_type, :display_name])
+    |> validate_required([:user_id, :type, :display_name])
+    |> validate_legal_form_orthogonality()
+  end
+
+  defp validate_legal_form_orthogonality(changeset) do
+    type = get_field(changeset, :type)
+
+    changeset
+    |> reject_unless(:legal_form, type == :bisnis, "hanya valid untuk Actor type :bisnis")
+    |> reject_unless(:mandate_type, type == :organisasi, "hanya valid untuk Actor type :organisasi")
+  end
+
+  defp reject_unless(changeset, _field, true, _msg), do: changeset
+
+  defp reject_unless(changeset, field, false, msg) do
+    case get_field(changeset, field) do
+      nil -> changeset
+      _ -> add_error(changeset, field, msg)
+    end
+  end
+end
