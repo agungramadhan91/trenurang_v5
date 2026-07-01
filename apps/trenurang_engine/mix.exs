999aaf20d43200dddaa4d24129dd78ad5b226571
agungramadhan91 <agungramadhans.work@gmail.com>
2026-07-01 15:35:18 +0700

    slice-2 phase 3: add dotenvy, bumblebee, axon, exla deps + .env infrastructure

--- apps/trenurang_engine/mix.exs
+++ apps/trenurang_engine/mix.exs
@@ -30,6 +30,10 @@ defmodule TrenurangEngine.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
       {:req, "~> 0.5"},
+      {:dotenvy, "~> 1.1"},
+      {:bumblebee, "~> 0.6"},
+      {:axon, "~> 0.7"},
+      {:exla, "~> 0.9"},
       {:trenurang_core, in_umbrella: true}
     ]
   end
